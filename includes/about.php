4829e711b77fa056e8c98311e5c9882d9bd0b277
xtr4nge <xtr4nge@gmail.com>
2015-07-19 10:54:55 +0100

    Update about.php

--- includes/about.php
+++ includes/about.php
@@ -1,4 +1,4 @@
 <b>Blue Pand</b> is a module created by AnguisCaptor to start and stop BlueTooth PAN (Personal Area Network)<br>
 Bluetooth Pan is used to share your device internet with FruityWifi, delivering internet without a ethernet cable or by wifi, but instead by Bluetooth
 <br><br>
-note: use <b>hcitool scan</b> command to get the macaddress.
+Note: use <b>hcitool scan</b> command to get the macaddress.
